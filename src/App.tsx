5885263b02860286350918693d0d3cc3942fc564
norahaggstrom <nora.haggstrom@gmail.com>
2026-05-20 20:02:04 +0200

    github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import StartPage from "./components/StartPage";
 import OmBinice from "./components/OmBinice";
@@ -8,7 +8,7 @@ import Resultat from "./components/Resultat";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<StartPage />} />
@@ -18,6 +18,6 @@ export default function App() {
           <Route path="/resultat" element={<Resultat />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Repot ligger på https://github.com/norahaggstrom/Binice
+// så sidan serveras från https://norahaggstrom.github.io/Binice/
+// Därför måste alla asset-länkar peka relativt /Binice/.
 export default defineConfig({
+  base: "/Binice/",
   plugins: [react()],
 });
